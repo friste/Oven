2deef72b18f2abbc8bee81a1b2312d0f730bf03c
August Lu <83371646+fristefristefriste@users.noreply.github.com>
2022-05-01 03:02:49 -0400

    yes

--- src/slider.js
+++ src/slider.js
@@ -1,31 +1,31 @@
 var slider = $("#slider");
 
 slider.roundSlider({
-    radius: 200,
-    width: 14,
-    svgMode: false,
-    pathColor: "#ee6161",
-    editableTooltip: false,
-    tooltipColor: "#000000",
-    rangeColor: "#e43b3b",
-    handleSize: "+8",
-    handleShape: "dot",
-    sliderType: "min-range",
-    startAngle: 90,
-    startValue: 10,
-    max: 120,
-    min: 10,
-    endValue: 120,
-    step: 10,
+  radius:200,
+  width:20,
+  svgMode: false,
+  pathColor: "#ee6161",
+  editableTooltip: false,
+  tooltipColor: "#000000",
+  rangeColor: "#e43b3b",
+  handleSize:"+8",
+  handleShape: "dot",
+  sliderType: "min-range",
+  startAngle: 90,
+  startValue: 10,
+  max:120,
+   min: 10,
+  endValue: 120,
+  step: 10,
 });
 
-$("#slider").roundSlider({ "rangeColor": "#ff8d00", "tooltipColor": "#ff8d00" });
-
+$("#slider").roundSlider({ "rangeColor": "#eb6262", "tooltipColor": "#FFFFFF" });
+    
 
 const confirmButton = document.getElementById("button")
 confirmButton.textContent = "Cook"
 confirmButton.onclick = function(ev) {
-    sessionStorage.setItem("duration", slider.getValue())
-        // alert("The timer duration will be " + sessionStorage.getItem("duration"))
-    window.location.href = "index.html"
+  sessionStorage.setItem("duration", slider.getValue())
+  // alert("The timer duration will be " + sessionStorage.getItem("duration"))
+  window.location.href = "index.html"
 }
